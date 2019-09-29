ea65e4984bd8a955c8ff184c63515423b838fd94
artyombakulev <i@artbakulev.com>
2019-09-29 12:18:41 +0300

    Fix bug

--- cmd/fileServer/main.go
+++ cmd/fileServer/main.go
@@ -10,6 +10,7 @@ import (
 
 func main() {
 	port := flag.String("p", "3000", "port to start file server")
+	flag.Parse()
 	router := mux.NewRouter()
 	log.Printf("Starting new file server on port :%s...", *port)
 	router.HandleFunc("/api/images/", handlers.UploadImageHandler).Methods(http.MethodPost)
