1bb135950dd25be961b35e3089fabeece96deb81
artyombakulev <i@artbakulev.com>
2019-09-29 13:02:35 +0300

    Fix path

--- configs/default.go
+++ configs/default.go
@@ -5,5 +5,5 @@ import "time"
 const CookieAuthName = "cinsear_session"
 const CookieAuthDuration = 10 * time.Hour
 
-const ImageUploadPath = "/var/www/media/"
+const ImageUploadPath = "/var/www/media/images/users/"
 const AccessLogPath = "../../"
