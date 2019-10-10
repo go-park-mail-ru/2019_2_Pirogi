9e6f1dca4eb2112be78a93aa72fea8492d55f188
artyombakulev <i@artbakulev.com>
2019-10-10 23:16:33 +0300

    change log paths

--- configs/default.go
+++ configs/default.go
@@ -8,8 +8,8 @@ const CookieAuthDuration = 10 * time.Hour
 // TODO: изменить путь для пользовательских картинок
 // const UsersImageUploadPath = "../../media/users/"
 
-const AccessLog = "/var/log/cinsear/access.log"
-const ErrorLog = "/var/log/cinsear/error.log"
+const AccessLog = "/cinsear/log/access.log"
+const ErrorLog = "/cinsear/log/error.log"
 
 const UsersImageUploadPath = "/var/www/media/images/users/"
 const FilmsImageUploadPath = "/var/www/media/images/films/"
