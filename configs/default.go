e27f9f90cb11274efceb544cba25d7d2af9a2598
artyombakulev <i@artbakulev.com>
2019-10-10 23:26:24 +0300

    Fix path

--- configs/default.go
+++ configs/default.go
@@ -8,11 +8,11 @@ const CookieAuthDuration = 10 * time.Hour
 // TODO: изменить путь для пользовательских картинок
 // const UsersImageUploadPath = "../../media/users/"
 
-const AccessLog = "/cinsear/log/access.log"
-const ErrorLog = "/cinsear/log/error.log"
+const AccessLog = "/log/access.log"
+const ErrorLog = "/log/error.log"
 
-const UsersImageUploadPath = "/var/www/media/images/users/"
-const FilmsImageUploadPath = "/var/www/media/images/films/"
+const UsersImageUploadPath = "/media/images/users/"
+const FilmsImageUploadPath = "/media/images/films/"
 
 const MongoDbUri = "mongodb://127.0.0.1:27017"
 const MongoDbName = "cinsear"
@@ -21,5 +21,5 @@ const FilmsCollectionName = "films"
 const CoockiesCollectionName = "usersAuthCookies"
 
 const APIPort = ":8000"
-const CertFile = "/cinsear/ssl/cert.pem"
-const KeyFile = "/cinsear/ssl/privkey.pem"
+const CertFile = "/ssl/cert.pem"
+const KeyFile = "/ssl/privkey.pem"
