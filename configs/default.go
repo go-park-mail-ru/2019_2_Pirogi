23ace28a000a59f3a9f57e6d6bece1cb9ee7c44a
Artefakt-ff <i@artbakulev.com>
2019-10-10 11:18:22 +0000

    Fix ssl

--- configs/default.go
+++ configs/default.go
@@ -8,11 +8,11 @@ const CookieAuthDuration = 10 * time.Hour
 // TODO: изменить путь для пользовательских картинок
 //const UsersImageUploadPath = "../../media/users/"
 
-const AccessLog = "../../../access.log"
-const ErrorLog = "../../../error.log"
+const AccessLog = "/log/access.log"
+const ErrorLog = "/log/error.log"
 
-const UsersImageUploadPath = "/var/www/media/images/users/"
-const FilmsImageUploadPath = "/var/www/media/images/films/"
+const UsersImageUploadPath = "/media/images/users/"
+const FilmsImageUploadPath = "/media/images/films/"
 
-const CertFile = "/etc/letsencrypt/live/cinsear.online/cert.pem"
-const KeyFile = "/etc/letsencrypt/live/cinsear.online/privkey.pem"
+const CertFile = "../../ssl/cert.pem"
+const KeyFile = "../../ssl/privkey.pem"
