4fd181eb92ac732c87035491888c0fac2a054d69
Elizaveta Prokhorova <lisa-bella97@mail.ru>
2019-10-15 23:53:19 +0300

    Fix script for running docker locally

--- scripts/run_production.sh
+++ scripts/run_production.sh
@@ -4,7 +4,7 @@ docker rmi cinsear
 docker volume create log
 docker volume create media
 docker volume create ssl
-docker build -t cinsear ../.
+docker build -t cinsear .
 docker run -ti -p 8000:8000 \
         --env mode=production \
         --mount type=volume,source=media,target=/media \
