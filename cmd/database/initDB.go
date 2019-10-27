ace364d0414b85e2957ba9c0891b85ed976c94ac
artyombakulev <i@artbakulev.com>
2019-10-27 15:03:18 +0300

    Fix fake fill db

--- cmd/database/initDB.go
+++ cmd/database/initDB.go
@@ -1,13 +1,17 @@
 package main
 
 import (
+	"flag"
 	"github.com/go-park-mail-ru/2019_2_Pirogi/internal/pkg/common"
 	"github.com/go-park-mail-ru/2019_2_Pirogi/internal/pkg/database"
 	"github.com/labstack/gommon/log"
 )
 
 func main() {
-	err := common.UnmarshalConfigs()
+	configsPath := flag.String("config", "../../configs/", "directory with configs")
+	flag.Parse()
+
+	err := common.UnmarshalConfigs(configsPath)
 	if err != nil {
 		log.Fatal(err.Error())
 	}
