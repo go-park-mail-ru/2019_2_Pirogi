582f24ebe7aea2a7ed3429fb4f098b5679003b3a
artyombakulev <i@artbakulev.com>
2019-10-31 13:34:08 +0300

    Fix bug

--- internal/pkg/database/database.go
+++ internal/pkg/database/database.go
@@ -7,13 +7,13 @@ import (
 
 type Database interface {
 	InsertOrUpdate(in interface{}) *models.Error
-	Get(id int, target string) (interface{}, *models.Error)
+	Get(id models.ID, target string) (interface{}, *models.Error)
 	Delete(in interface{}) *models.Error
 	CheckCookie(cookie *http.Cookie) bool
 	FindUserByEmail(email string) (models.User, bool)
-	FindUserByID(id int) (models.User, bool)
+	FindUserByID(id models.ID) (models.User, bool)
 	FindUserByCookie(cookie *http.Cookie) (models.User, bool)
 	FindFilmByTitle(title string) (models.Film, bool)
-	FindFilmByID(id int) (models.Film, bool)
+	FindFilmByID(id models.ID) (models.Film, bool)
 	ClearDB()
 }
