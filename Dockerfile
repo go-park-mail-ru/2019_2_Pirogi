7dd778fae46593424bb684412420190b08838c94
artyombakulev <i@artbakulev.com>
2019-10-31 16:36:56 +0300

    Remove repeatedly initDB when starting Add option -first-time for first start (run.sh)

--- Dockerfile
+++ Dockerfile
@@ -5,9 +5,6 @@ RUN mkdir -p /media/images/films
 RUN mkdir -p /log
 WORKDIR /cinsear
 COPY . .
-WORKDIR /cinsear/cmd/database
-RUN go build -o initDB .
-CMD ["/cinsear/cmd/database/initDB"]
 WORKDIR /cinsear/cmd/server
 RUN go build -o main .
 CMD ["/cinsear/cmd/server/main"]
--- scripts/run.sh
+++ scripts/run.sh
@@ -1,10 +1,20 @@
+if [ "$1" = "-first-time" ]; then
+  echo "---Filling db..."
+  cd ../cmd/database/ && go run initDB.go
+fi
+
+echo "---Stopping containers"
 docker stop cinsear
 docker rm cinsear
 docker stop cinsear-db
 docker rm cinsear-db
 
+
+echo "---Checking or creating volumes..."
 docker volume create media
 docker volume create log
 docker volume create db
 
+echo "---Starting..."
 docker-compose up --build --detach server mongo
+
