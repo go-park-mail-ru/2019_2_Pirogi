e965584926d54929a35ee505538f85434ed490b4
Elizaveta Prokhorova <lisa-bella97@mail.ru>
2019-11-02 00:46:43 +0300

    Fix fake fill db

--- cmd/database/initDB.go
+++ cmd/database/initDB.go
@@ -42,17 +42,55 @@ func FakeFillDB(conn *database.MongoConnection) {
 		Description: "Терзаемый хронической бессонницей и отчаянно пытающийся вырваться из мучительно скучной жизни " +
 			"клерк встречает некоего Тайлера Дардена, харизматического торговца мылом с извращенной философией. Тайлер " +
 			"уверен, что самосовершенствование — удел слабых, а саморазрушение — единственное, ради чего стоит жить.",
-		Year:      1999,
+		Year:      "1999",
 		Countries: []string{"США", "Германия"},
 		Genres:    []models.Genre{"триллер", "драма", "криминал"},
-		Actors: []models.PersonTrunc{{0, "Эдвард Нортон"}, {1, "Брэд Питт"},
-			{2, "Хелена Бонем Картер"}},
-		Directors:     []models.PersonTrunc{{3, "Дэвид Финчер"}},
-		Producers:     []models.PersonTrunc{{4, "Росс Грэйсон Белл"}, {5, "Сиан Чаффин"}},
-		Compositors:   []models.PersonTrunc{{6, "Даст Бразерс"}, {7, "Джон Кинг"}},
-		Screenwriters: []models.PersonTrunc{{8, "Джим Улс"}, {9, "Чак Паланик"}},
+		Actors: []models.PersonTrunc{{0, "Эдвард Нортон", 4.5}, {1, "Брэд Питт", 4.8},
+			{2, "Хелена Бонем Картер", 4.0}},
+		Directors:     []models.PersonTrunc{{3, "Дэвид Финчер", 3.8}},
+		Producers:     []models.PersonTrunc{{4, "Росс Грэйсон Белл", 4.7}, {5, "Сиан Чаффин", 4.5}},
+		Compositors:   []models.PersonTrunc{{6, "Даст Бразерс", 4.1}, {7, "Джон Кинг", 4.2}},
+		Screenwriters: []models.PersonTrunc{{8, "Джим Улс", 3.3}, {9, "Чак Паланик", 4.7}},
 		Poster:        models.Image{0, "matrix.png"},
 		Images:        []models.Image{},
+	})
+
+	conn.Upsert(models.NewReview{
+		Title:    "Best review title",
+		Body:     "Best review body",
+		FilmID:   0,
+		AuthorID: 1,
+	})
+
+	conn.Upsert(models.NewReview{
+		Title:    "Best review title 2",
+		Body:     "Best review body 2",
+		FilmID:   0,
+		AuthorID: 2,
+	})
+
+	conn.Upsert(models.Stars{
+		UserID: 0,
+		FilmID: 0,
+		Mark:   3.0,
+	})
+
+	conn.Upsert(models.Stars{
+		UserID: 1,
+		FilmID: 0,
+		Mark:   5.0,
+	})
+
+	conn.Upsert(models.Like{
+		UserID:   0,
+		Target:   "film",
+		TargetID: 0,
+	})
+
+	conn.Upsert(models.Like{
+		UserID:   1,
+		Target:   "review",
+		TargetID: 0,
 	})
 }
 
