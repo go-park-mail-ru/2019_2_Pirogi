1c3b447ae5773a132e19a7a85cbb74a8659004c2
artyombakulev <i@artbakulev.com>
2019-11-08 01:49:49 +0300

    Fix cors

--- internal/pkg/server/server.go
+++ internal/pkg/server/server.go
@@ -89,7 +89,7 @@ func CreateAPIServer(conn database.Database) (*echo.Echo, error) {
 
 	e.Use(echoMid.CORSWithConfig(echoMid.CORSConfig{
 		AllowOrigins: []string{"https://cinsear.ru", "http://localhost:8080"},
-		AllowHeaders: []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept},
+		AllowHeaders: []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept, echo.HeaderAccessControlAllowCredentials},
 	}))
 	e.Use(echoMid.Secure())
 	e.Use(middleware.SetCSRFCookie)
