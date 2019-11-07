757969082730d09f2ac2ad3fe483e8a8c39bb27b
artyombakulev <i@artbakulev.com>
2019-11-08 00:26:31 +0300

    Fix

--- scripts/runFromGit.sh
+++ scripts/runFromGit.sh
@@ -1,7 +1,9 @@
 if [ "$1" != "-b" ] || [ -z $"$2" ]; then
-  echo "No branch defined. Use flag -b to define the branch to pull from"
+  echo "pulling from origin..."
+  git pull origin
 else
+  echo "pulling from origin $2..."
   git pull origin "$2"
-  easyjson --all ./internal/pkg/models
-  ./scripts/run.sh
 fi
+easyjson --all ./internal/pkg/models
+./scripts/run.sh
