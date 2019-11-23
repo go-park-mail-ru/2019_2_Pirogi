ba7e2e7d3e8702a837951bfadca1a14c2673c77e
artyombakulev <i@artbakulev.com>
2019-11-23 18:05:11 +0300

    fix

---
+++ cmd/chat/chatAdmin.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"encoding/json"
+	"github.com/go-park-mail-ru/2019_2_Pirogi/app/domain/model"
+	"github.com/go-park-mail-ru/2019_2_Pirogi/app/infrastructure/database"
+	"github.com/go-park-mail-ru/2019_2_Pirogi/configs"
+	json2 "github.com/go-park-mail-ru/2019_2_Pirogi/pkg/json"
+	"log"
+	"net/http"
+)
+
+func main() {
+	conn, err := database.InitMongo(configs.Default.MongoHost)
+	if err != nil {
+		log.Fatal(err)
+	}
+	adminServer := http.NewServeMux()
+	adminServer.HandleFunc("/admin", getHandlerAdmin(conn))
+	log.Fatal(http.ListenAndServe(":9000", adminServer))
+}
+
+func getHandlerAdmin(conn database.Database) func(res http.ResponseWriter, req *http.Request) {
+	return func(res http.ResponseWriter, req *http.Request) {
+		chatsInterfaces, err := conn.GetAll(configs.Default.ChatTargetName)
+		if err != nil {
+			http.Error(res, "no chats", 404)
+			return
+		}
+		var chats [][]byte
+		for _, chatInterface := range chatsInterfaces {
+			chat := chatInterface.(model.Chat)
+			body, err := json.Marshal(chat)
+			if err != nil {
+				continue
+			}
+			chats = append(chats, body)
+
+		}
+		res.Write(json2.MakeJSONArray(chats))
+	}
+}
--- cmd/chat/main.go
+++ cmd/chat/main.go
@@ -1,14 +1,11 @@
 package main
 
 import (
-	"encoding/json"
 	"flag"
-	"github.com/go-park-mail-ru/2019_2_Pirogi/app/domain/model"
 	"github.com/go-park-mail-ru/2019_2_Pirogi/app/infrastructure/database"
 	"github.com/go-park-mail-ru/2019_2_Pirogi/chat"
 	"github.com/go-park-mail-ru/2019_2_Pirogi/configs"
 	"github.com/go-park-mail-ru/2019_2_Pirogi/pkg/configuration"
-	json2 "github.com/go-park-mail-ru/2019_2_Pirogi/pkg/json"
 	"go.uber.org/zap"
 	"log"
 	"net/http"
@@ -46,30 +43,5 @@ func main() {
 
 	server := chat.NewServer("/", conn)
 	go server.Listen()
-	go log.Fatal(http.ListenAndServe(":8080", nil))
-	adminServer := http.NewServeMux()
-	adminServer.HandleFunc("/admin", getHandlerAdmin(conn))
-	log.Fatal(http.ListenAndServe(":9000", adminServer))
-
-}
-
-func getHandlerAdmin(conn database.Database) func(res http.ResponseWriter, req *http.Request) {
-	return func(res http.ResponseWriter, req *http.Request) {
-		chatsInterfaces, err := conn.GetAll(configs.Default.ChatTargetName)
-		if err != nil {
-			http.Error(res, "no chats", 404)
-			return
-		}
-		var chats [][]byte
-		for _, chatInterface := range chatsInterfaces {
-			chat := chatInterface.(model.Chat)
-			body, err := json.Marshal(chat)
-			if err != nil {
-				continue
-			}
-			chats = append(chats, body)
-
-		}
-		res.Write(json2.MakeJSONArray(chats))
-	}
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
