80a254c08e014b73f9a760d3b2d2902863d71b50
Elizaveta Prokhorova <lisa-bella97@mail.ru>
2019-11-23 12:21:19 +0300

    Add docker support for chat

---
+++ scripts/runChat.sh
@@ -0,0 +1,16 @@
+echo "---Stopping containers"
+docker stop cinsear-chat
+docker rm cinsear-chat
+
+echo "---Checking or creating volumes..."
+docker volume create media
+docker volume create log
+docker volume create db
+
+echo "---Starting..."
+docker-compose up --build --detach chat
+
+if [ "$1" = "-first-time" ]; then
+  echo "---Filling db..."
+  go run cmd/database/initDB.go
+fi
