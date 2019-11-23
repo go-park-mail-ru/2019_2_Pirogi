0ee6af064838277991bd42a37d40cd38a94867fe
artyombakulev <i@artbakulev.com>
2019-11-23 17:53:13 +0300

    fix

--- cmd/chat/main.go
+++ cmd/chat/main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
+	"github.com/go-park-mail-ru/2019_2_Pirogi/app/domain/model"
 	"github.com/go-park-mail-ru/2019_2_Pirogi/app/infrastructure/database"
 	"github.com/go-park-mail-ru/2019_2_Pirogi/chat"
 	"github.com/go-park-mail-ru/2019_2_Pirogi/configs"
 	"github.com/go-park-mail-ru/2019_2_Pirogi/pkg/configuration"
+	myjson "github.com/go-park-mail-ru/2019_2_Pirogi/pkg/json"
 	"go.uber.org/zap"
 	"log"
 	"net/http"
@@ -40,18 +43,29 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	adminServer := http.NewServeMux()
-	adminServer.HandleFunc("/admin", getHandlerAdmin(conn))
-	go log.Fatal(http.ListenAndServe(":9000", adminServer))
+	//adminServer := http.NewServeMux()
+	//adminServer.HandleFunc("/admin", getHandlerAdmin(conn))
+	//go log.Fatal(http.ListenAndServe(":9000", adminServer))
 
 	server := chat.NewServer("/", conn)
 	go server.Listen()
 	log.Fatal(http.ListenAndServe(":8080", nil))
 
 }
-
-func getHandlerAdmin(conn database.Database) func(res http.ResponseWriter, req *http.Request) {
-	return func(res http.ResponseWriter, req *http.Request) {
-		res.Write([]byte("hello"))
-	}
-}
+//
+//func getHandlerAdmin(conn database.Database) func(res http.ResponseWriter, req *http.Request) {
+//	return func(res http.ResponseWriter, req *http.Request) {
+//		chatsInterfaces, err := conn.GetAll(configs.Default.ChatTargetName)
+//		if err != nil {
+//			http.Error(res, "no chats", 404)
+//			return
+//		}
+//		var chats [][]byte
+//		for _, chatInterface := range chatsInterfaces {
+//			chat  = chatInterface.(model.Chat)
+//			chats = append(chats,  json.Marshal(chat))
+//
+//		}
+//		json.MakeJSONArray(chats)
+//	}
+//}
