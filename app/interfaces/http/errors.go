5eaec00597db28980508282651efe5848db22145
artyombakulev <i@artbakulev.com>
2019-11-24 18:25:29 +0300

    fix logging

--- app/interfaces/http/errors.go
+++ app/interfaces/http/errors.go
@@ -4,9 +4,7 @@ import (
 	"github.com/go-park-mail-ru/2019_2_Pirogi/app/domain/model"
 	"github.com/labstack/echo"
 	"go.uber.org/zap"
-	"go.uber.org/zap/zapcore"
 	"net/http"
-	"time"
 )
 
 func GetHTTPErrorHandler(logger *zap.Logger) func(err error, ctx echo.Context) {
@@ -21,12 +19,12 @@ func GetHTTPErrorHandler(logger *zap.Logger) func(err error, ctx echo.Context) {
 			e.Error = he.Message.(string)
 		}
 
-		fields := []zapcore.Field{
-			zap.Int("status", e.Status),
-			zap.String("time", time.Now().String()),
-			zap.String("message", e.Error),
-		}
-		logger.Error("Error: ", fields...)
+		//fields := []zapcore.Field{
+		//	zap.Int("status", e.Status),
+		//	zap.String("time", time.Now().String()),
+		//	zap.String("message", e.Error),
+		//}
+		logger.Error("Error: ")
 		err = ctx.JSON(e.Status, e)
 	}
 }
