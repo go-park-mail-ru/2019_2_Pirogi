f4f8dc7d512dc7dd033afc00647c8160c5fd3013
Artefakt-ff <bakulev.artyom@yandex.ru>
2019-12-27 04:06:39 +0300

    fix ci deploy

--- scripts/runGo.sh
+++ scripts/runGo.sh
@@ -6,7 +6,7 @@ docker stop cinsear
 docker rm cinsear
 
 echo "---Parsing models..."
-easyjson -all -pkg app/domain/model
+easyjson --all -pkg app/domain/model
 
 echo "---Starting..."
 docker-compose up --build --detach server
